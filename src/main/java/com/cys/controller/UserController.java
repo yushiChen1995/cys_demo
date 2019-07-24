3aa99bcf56944934a4ab8518f02bfe2b63f9eb6b
chenyushi <chenyushi@lechebang.com>
2019-07-24 10:00:03 +0800

    修改haha

--- src/main/java/com/cys/controller/UserController.java
+++ src/main/java/com/cys/controller/UserController.java
@@ -32,6 +32,6 @@ public class UserController {
 
     @GetMapping("test1")
     public ResponseEntity<String> test1(){
-        return ResponseEntity.ok("hello,jenkins---cys---");
+        return ResponseEntity.ok("hello,jenkins---haha---");
     }
 }
